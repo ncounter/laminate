898c90cfc8ffadcd9dbe60e1c4bb60550678ecd9
dleidi <dleidi@suse.com>
2018-10-26 16:15:05 +0200

    Add npm module version in a subfolder

---
+++ loggerhead-module/README.md
@@ -0,0 +1,111 @@
+# Loggerhead
+A simple plain Javascript plugin to log frontend messages to a server.
+
+## How it works
+`Loggerhead.js` sends a log message to a configurable endpoint URL by a POST request each time one of the `log level` function is called.
+
+### Log levels
+Log levels are
+* `info`
+* `debug`
+* `warning`
+* `error`
+
+By default all levels are enabled. Whenever a log level function is called, it receives a string message, it prepares a `POST` request and it forwards the message to the set `URL`, injecting in the `POST` data the `log level` information.
+
+**Note**: each log level function returns the `Promise` that sends the `POST` request, this way it is possible to add a `.then()` slice in order to apply some other action on the `response` (if any) from the server after storing the log message. See an example below:
+```javascript
+Lh.info('Send this info log message to the server')
+    .then(serverResponse => alert(serverResponse.message));
+```
+
+### Events
+By default `Loggerhead.js` handles also some `window event` (the typical types a user wants to store a log for) by pre-configured `listener`s: they callback a dedicated built-in function per each type of `event`.
+
+Types of `events` it listens for are:
+* `load`: it sends an `info` log level message containing the `DateTime` and the current `URL` page the browser loads
+* `unload`: same behavior of the `load` listener, but for unloading pages
+* `error`: it sends an `error` log level message containing some details of the error event that just happened
+
+These built-in functions are named following the pattern `<eventName>EventListener` (e.g.: [`loadEventListener`, `unloadEventListener`, `errorEventListener`]), they receive the `event` object as a parameter, and they can be overridden like the following:
+
+```javascript
+Lh.loadEventListener = (event) => console.log(event);
+Lh.unloadEventListener = (event) => console.log(event);
+Lh.errorEventListener = (event) => alert(event.message);
+```
+
+### Headers
+`Loggerhead.js` provides also a way to customize `headers` values of the `POST` request: this can be used to add some **authentication** parameters, for instance. The default method behaves like a `proxy` receiving and returning the default `headers` map (*). In order to add more `headers` parameters this method can be overridden by a function that receives and returns the `headers` map as well, but it does something in the middle. See below an example:
+
+```javascript
+// this is the default method
+Lh.setHeaders = function(headers) {
+  return headers;
+}
+
+// this overrides the default method adding the `X-CSRF-Token` parameters in the `headers` map
+Lh.setHeaders = function(headers) {
+  headers.set('X-CSRF-Token', '<my-token-value>');
+  return headers;
+}
+```
+
+(*) Note: by default `headers` contains only `{'Content-Type': 'application/json; charset=utf-8')}`
+
+## How to use
+```html
+<script type="text/javascript" src="loggerhead.js"></script>
+
+<script type="text/javascript">
+  /* Minimal code to get Loggerhead working properly */
+  const Lh = window.Loggerhead;
+  Lh.set({ url: 'https://httpbin.org/post' });
+
+  /* Let's use Loggerhead functions to send some log messages */
+  Lh.info('This is an info log message');
+  Lh.warning('This is an warning log message')
+      .then(confirm => alert(confirm));
+
+  /* Let's disable debug log level */
+  Lh.set({ levels: { debug : false }});
+</script>
+```
+
+## Config parameters
+```javascript
+// the server endpoint where to send logs
+url: String,
+
+// log levels, enabled by default
+levels: {
+  info: Boolean,
+  debug: Boolean,
+  warning: Boolean,
+  error: Boolean,
+}
+
+// built-in event handlers, enabled by default
+events: {
+  load: Boolean,
+  unload: Boolean,
+  error: Boolean,
+}
+```
+
+Parameters are configurable passing a *partial* or *complete* config object with desired values to the `set` method:
+
+```javascript
+Lh.set(
+  {
+    url: 'http://myserver.com/my-frontend-log-endpoint',
+    levels: {
+      debug: false,
+      warning: false,
+    },
+    events: {
+      unload: false
+    }
+  }
+);
+```
---
+++ loggerhead-module/loggerhead-module.js
@@ -0,0 +1,121 @@
+(function(window){
+  // 'use strict';
+
+  function Loggerhead(){
+    var _loggerheadObject = {};
+
+    // configuration object
+    var config = {
+      url: '',
+      levels: {info: true, debug: true, warning: true, error: true},
+      events: {load: true, unload: true, error: true},
+    };
+
+    _loggerheadObject.setHeaders = function(headers) {
+      return headers;
+    }
+
+    // private functions
+    function formatPostDataErrorMessage(opts, error) {
+      return 'Error trying to send log message to `' + config.url + '`\n\n' +
+          'POST JSON data was = ' + JSON.stringify(opts) + '\n\n' + error;
+    }
+    function postData(data) {
+      var headers = new Map();
+      headers.set('Content-Type', 'application/json; charset=utf-8');
+      headers = _loggerheadObject.setHeaders(headers);
+
+      const opts = {
+        method: 'POST', mode: 'cors', cache: 'no-cache', credentials: 'same-origin',
+        headers: headers,
+        redirect: 'follow', referrer: 'no-referrer',
+        body: JSON.stringify(data),
+      }
+      const result = fetch(config.url, opts)
+          .then(function (response) { return response.json();})
+          .catch(error =>
+            {
+              const errorMessage = formatPostDataErrorMessage(opts, error);
+              console.error(errorMessage, error);
+            }
+          );
+      return result;
+    }
+
+    // log level functions
+    _loggerheadObject.info = function(message) {
+      var ret = new Promise(function(resolve, reject) { resolve() });
+      if (config.levels.info) {
+        ret = postData({'level' : 'info', 'message' : message});
+      }
+      return ret;
+    }
+    _loggerheadObject.debug = function(message) {
+      var ret = new Promise(function(resolve, reject) { resolve() });
+      if (config.levels.debug) {
+        ret = postData({'level' : 'debug', 'message' : message});
+      }
+      return ret;
+    }
+    _loggerheadObject.warning = function(message) {
+      var ret = new Promise(function(resolve, reject) { resolve() });
+      if (config.levels.warning) {
+        ret = postData({'level' : 'warning', 'message' : message});
+      }
+      return ret;
+    }
+    _loggerheadObject.error = function(message) {
+      var ret = new Promise(function(resolve, reject) { resolve() });
+      if (config.levels.error) {
+        ret = postData({'level' : 'error', 'message' : message});
+      }
+      return ret;
+    }
+
+    // configuration parameters setter
+    _loggerheadObject.set = function(configObject) {
+      const configMap = new Map(Object.entries(configObject));
+      Array.from(configMap.keys()).map(k => {
+        if (configMap.get(k) instanceof Object) {
+          const subConfigMap = new Map(Object.entries(configMap.get(k)));
+          Array.from(subConfigMap.keys()).map(kv => {
+            config[k][kv] = subConfigMap.get(kv);
+          });
+        }
+        else {
+          config[k] = configMap.get(k)
+        }
+      });
+    }
+
+    // built-in event listeners
+    _loggerheadObject.loadEventListener = function(event) {
+      if (config.events.load) {
+        this.info('[' + new Date().toUTCString() + '] - Loading `' + window.location + '`');
+      }
+    }
+    _loggerheadObject.unloadEventListener = function(event) {
+      if (config.events.unload) {
+        this.info('[' + new Date().toUTCString() + '] - Leaving `' + window.location + '`');
+      }
+    }
+    _loggerheadObject.errorEventListener = function(event) {
+      if (config.events.error) {
+        // Note that col & error are new to the HTML 5 and may not be supported in every browser.
+        var extra = !event.colno ? '' : '\ncolumn: ' + event.colno;
+        extra += !event.error ? '' : '\nerror: ' + event.error;
+
+        const errorMessage = event.message + '\nurl: ' + event.filename + '\nline: ' + event.lineno + extra;
+        this.error(errorMessage);
+      }
+    }
+    return _loggerheadObject;
+  }
+
+  if(typeof(window.Loggerhead) === 'undefined'){
+    window.Loggerhead = Loggerhead();
+    window.addEventListener('load', function(event) { window.Loggerhead.loadEventListener(event) });
+    window.addEventListener('unload', function(event) { window.Loggerhead.unloadEventListener(event) });
+    window.addEventListener('error', function(event) { window.Loggerhead.errorEventListener(event) });
+  }
+})(window);
