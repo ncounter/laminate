c1d6257836d13fcbc534598d35396754001821a5
dleidi <dleidi@suse.com>
2018-10-29 10:01:53 +0100

    Add a script to adapt and update the npm-module

---
+++ update-npm-module.sh
@@ -0,0 +1,21 @@
+#!/usr/bin
+
+# adapt the .js to be an npm module
+cp ./loggerhead.js ./loggerhead-module/loggerhead-module.js
+echo "" >> ./loggerhead-module/loggerhead-module.js
+echo "exports.Loggerhead = Loggerhead;" >> ./loggerhead-module/loggerhead-module.js
+
+# adapt the README with the usage for an npm module
+sed "/\/\* Minimal code to get Loggerhead working properly \*\//a \ \ var Loggerhead = require('loggerhead-module').Loggerhead;" ./README.md > loggerhead-module/README.md
+
+cd ./loggerhead-module
+npm version patch
+
+echo "---"
+echo "If everything looks good, then don't forget to **publish** the updated npm module!!"
+echo "You can easily do it with the following commands:"
+echo "> cd ./loggerhead-module"
+echo "> npm publish"
+echo "---"
+
+exit
